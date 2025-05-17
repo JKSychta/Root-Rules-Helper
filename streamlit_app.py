c66e22982abaa84920907c4d3b8ec14d9ce79d71
JKSychta <83690016+JKSychta@users.noreply.github.com>
2025-05-17 14:44:03 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import docloader
 import chat_openrouter
 import embedder
 from langchain_huggingface import HuggingFaceEmbeddings
-from langchain_core import ChatPromptTemplates
+import langchain_core
 
 st.set_page_config(layout="wide", page_title="OpenRouter chatbot app")
 st.title("OpenRouter chatbot app")
